62e98e52f365bb15db230a32d9edcf92dd4b70e9
Adam CHEN <jinghao_chen@hotmail.com>
2024-11-28 00:48:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 from utils import load_chain
 
 # Custom image for the app icon and the assistant's avatar
-company_logo = 'https://blendle.com/img/packaging/android-touch-icon-196x196.png'
+PROFILE = 'CHEN_JINGHAO.jpg'
 
 # Configure streamlit page
 st.set_page_config(
-    page_title="Your Notion Chatbot",
-    page_icon=company_logo
+    page_title="Interview with Adam JINGHAO CHEN",
+    page_icon=PROFILE
 )
 
 # Initialize LLM chain in session_state
@@ -19,27 +19,27 @@ if 'chain' not in st.session_state:
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant", 
-                                  "content": "Hi human! I am Blendle's smart AI. How can I help you today?"}]
+                                  "content": "Good day! :raised_hand_with_fingers_splayed: This is Adam JINGHAO CHEN. How can I help you today?"}]
 
 # Display chat messages from history on app rerun
 # Custom avatar for the assistant, default avatar for user
 for message in st.session_state.messages:
     if message["role"] == 'assistant':
-        with st.chat_message(message["role"], avatar=company_logo):
+        with st.chat_message(message["role"], avatar=PROFILE):
             st.markdown(message["content"])
     else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 # Chat logic
-if query := st.chat_input("Ask me anything"):
+if query := st.chat_input("Ask me anything about my experience, skills, and qualifications!"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": query})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(query)
 
-    with st.chat_message("assistant", avatar=company_logo):
+    with st.chat_message("assistant", avatar=PROFILE):
         message_placeholder = st.empty()
         # Send user's question to our chain
         result = st.session_state['chain']({"question": query})
